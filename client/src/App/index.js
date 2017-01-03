60ca7befd5c5e618ef1610e9e808cf80754eb3fc
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-03 01:06:31 -0800

    Responsively resizes according to breakpoints from window

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import Grid from 'react-grid-layout';
+import { Responsive, WidthProvider } from 'react-grid-layout';
+const Grid = WidthProvider(Responsive);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      layouts: {
+        lg: [
+          {i: 'a', x: 0, y: 0, w: 3, h: 2, minW: 2, maxW: 4, maxH: 4},
+          {i: 'b', x: 3, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+          {i: 'c', x: 0, y: 3, w: 3, h: 2}
+        ]
+      },
+      rowHeight: 100,
+    }
+  }
+
+  resizeHeight(newBreakpoint, newCols) {
+    debugger;
+    console.log('breakpoint changed!');
+    console.log('newBreakpoint:', newBreakpoint);
+    console.log('newCols:', newCols);
+    var heights = {
+      lg: 100,
+      md: 83,
+      sm: 64,
+      xs: 40,
+      xxs: 20,
+    }
+    this.setState({ rowHeight: heights[newBreakpoint] });
   }
 
   render() {
-    var layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
-    ];
     return (
       <div>
         <h1>Hello World</h1>
-        <Grid className='layout' layout={layout} cols={12} rowHeight={30} width={1200}>
+        <Grid className='layout' layouts={this.state.layouts}
+        breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 240}}
+        cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
+        onBreakpointChange={this.resizeHeight.bind(this)}
+        rowHeight={this.state.rowHeight}>
           <div key={'a'}>a</div>
           <div key={'b'}>b</div>
           <div key={'c'}>c</div>
