e950835b065faf71f2b768ed73fa348a5d38291d
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-02 20:03:29 -0800

    Initializes build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 var webpack = require('webpack');
 
 var BUILD_DIR = `${__dirname}/public`;
-var APP_DIR = `${__dirname}/client/App`;
+var APP_DIR = `${__dirname}/client/src/App`;
 
 var config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel'
+      }
+    ]
   }
 };
 
