57e432962f9a42e8379d040027166fa4fdbdac10
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-02 20:08:51 -0800

    Exports App component for build

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { Render } from 'react-dom';
-import App from 'App'
+import App from './App'
 
 Render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var webpack = require('webpack');
 
-var BUILD_DIR = `${__dirname}/public`;
-var APP_DIR = `${__dirname}/client/src/App`;
+var BUILD_DIR = `${__dirname}/public/dist`;
+var APP_DIR = `${__dirname}/client/src/`;
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
