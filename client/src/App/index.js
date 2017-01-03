4d5b2e6f659e8f87fae68b70d51e45efaeacd646
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-03 02:44:24 -0800

    Persists temporarily as server is running

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -5,26 +5,42 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      layout: [
-        {i: 'a', x: 0, y: 0, w: 8, h: 4, maxW: 8, maxH: 8},
-        {i: 'b', x: 0, y: 4, w: 4, h: 4, maxW: 8, maxH: 8},
-        {i: 'c', x: 4, y: 4, w: 4, h: 4, maxW: 8, maxH: 8}
-      ]
+      layout: []
     }
 
     this.updateLayout = this.updateLayout.bind(this);
+  }
+
+  componentDidMount() {
+    this.getPage();
   }
 
   getPage() {
     var app = this;
     fetch('/api/page')
       .then(response => response.json())
+      .then(layout => {
+        console.log(layout);
+        app.setState({ layout });
+      });
+  }
+
+  postPage(layout) {
+    var app = this;
+    var body = { layout }
+    fetch('/api/page', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+      .then(response => response.json())
       .then(layout => app.setState({ layout }))
   }
 
   updateLayout(layout) {
-    this.setState({ layout });
-    console.log(this.state.layout);
+    this.postPage(layout);
   }
 
   render() {
@@ -32,11 +48,12 @@ class App extends React.Component {
       <div>
         <h1>Hello World</h1>
         <Grid className='layout' layout={this.state.layout}
-        onLayoutChange={this.updateLayout} cols={8} rowHeight={80} width={640}
-        verticalCompact={false}>
-          <div key={'a'}>a</div>
-          <div key={'b'}>b</div>
-          <div key={'c'}>c</div>
+        onLayoutChange={this.updateLayout} cols={8} rowHeight={80} width={640}>
+          {this.state.layout.map(panel => {
+            return (
+              <div key={panel.i}>{panel.i}</div>
+            )
+          })}
         </Grid>
       </div>
     )
--- server/index.js
+++ server/index.js
@@ -1,8 +1,38 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var router = express.Router();
 var port = process.env.PORT || 3000;
 
 app.use('/', express.static(`${__dirname}/../public`));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', router);
+
+router.use((req, res, next) => {
+  console.log('Time:', Date.now());
+  next();
+});
+
+var tempStorage = [
+  {i: 'a', x: 0, y: 0, w: 8, h: 4, maxW: 8, maxH: 8},
+  {i: 'b', x: 0, y: 4, w: 4, h: 4, maxW: 8, maxH: 8},
+  {i: 'c', x: 4, y: 4, w: 4, h: 4, maxW: 8, maxH: 8},
+];
+
+router.route('/page')
+  .get((req, res) => {
+    console.log('getting');
+    res.json(tempStorage)
+  })
+  .post((req, res) => {
+    console.log('posting');
+    let layout = req.body.layout;
+    tempStorage = layout;
+    console.log(tempStorage);
+    res.json(tempStorage);
+  });
 
 app.listen(port, () => {
   console.log('Currently listening on port:', port);
