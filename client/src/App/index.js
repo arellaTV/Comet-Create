e27251c0f4b2435022393deac9317b870632e905
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-03 02:06:42 -0800

    Restores to static layout

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,42 +1,34 @@
 import React from 'react';
-import { Responsive, WidthProvider } from 'react-grid-layout';
-const Grid = WidthProvider(Responsive);
+import Grid from 'react-grid-layout';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      layouts: {
-        lg: [
-          {i: 'a', x: 0, y: 0, w: 3, h: 2, minW: 2, maxW: 4, maxH: 4},
-          {i: 'b', x: 3, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-          {i: 'c', x: 0, y: 3, w: 3, h: 2}
-        ]
-      },
-      rowHeight: 100,
+      layout: [
+        {i: 'a', x: 0, y: 0, w: 8, h: 4, maxW: 8, maxH: 8},
+        {i: 'b', x: 0, y: 4, w: 4, h: 4, maxW: 8, maxH: 8},
+        {i: 'c', x: 4, y: 4, w: 4, h: 4, maxW: 8, maxH: 8}
+      ]
     }
+
+    this.updateLayout = this.updateLayout.bind(this);
   }
 
-  resizeHeight(newBreakpoint, newCols) {
-    var heights = {
-      lg: 100,
-      md: 83,
-      sm: 64,
-      xs: 40,
-      xxs: 20,
-    }
-    this.setState({ rowHeight: heights[newBreakpoint] });
+  getPage() {}
+
+  updateLayout(layout) {
+    this.setState({ layout });
+    console.log(this.state.layout);
   }
 
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <Grid className='layout' layouts={this.state.layouts}
-        breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 240}}
-        cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
-        onBreakpointChange={this.resizeHeight.bind(this)}
-        rowHeight={this.state.rowHeight}>
+        <Grid className='layout' layout={this.state.layout}
+        onLayoutChange={this.updateLayout} cols={8} rowHeight={80} width={640}
+        verticalCompact={false}>
           <div key={'a'}>a</div>
           <div key={'b'}>b</div>
           <div key={'c'}>c</div>
